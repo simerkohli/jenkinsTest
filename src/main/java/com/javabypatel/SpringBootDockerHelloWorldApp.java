c1b090cb99957cf86c1ca72f8b4f6c1229798660
Simerpreet singh kohli <44640633+simerkohli@users.noreply.github.com>
2022-08-09 23:14:33 +0530

    Add files via upload

---
+++ src/main/java/com/javabypatel/SpringBootDockerHelloWorldApp.java
@@ -0,0 +1,12 @@
+package com.javabypatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerHelloWorldApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerHelloWorldApp.class, args);
+    }
+}
